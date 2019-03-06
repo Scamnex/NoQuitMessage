4b8475cafde5d7d9203fa8e0faa3b4bb3cc37e2d
Scamnex <48050233+Scamnex@users.noreply.github.com>
2019-03-06 20:32:56 +0100

    Create Main.php

---
+++ src/Scamnex/Main.php
@@ -0,0 +1,25 @@
+<?php
+namespace Scamnex;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as T;
+class Main extends PluginBase implements Listener
+{
+    public function onEnable()
+    {
+        $this->getLogger()->info(T::AQUA . "Aktiviert");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+    public function onDisable()
+    {
+        $this->getLogger()->info(T::AQUA . "Deaktiviert");
+    }
+    public function onQuit (PlayerQuitEvent $e)
+    {
+        $player = $e->getPlayer();
+        $name = $player->getName();
+        $e->setQuitMessage("");
+    }
+}
